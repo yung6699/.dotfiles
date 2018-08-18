f2b0df82b1d14f5193b8d05c7d0707a53a732a46
Yoon <yty3592@gmail.com>
2018-08-19 02:29:16 +0900

    개인 dotfile 수정

--- default-install.sh
+++ default-install.sh
@@ -2,21 +2,27 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew doctor
+brew install curl wget
 
 # cask 설치
 brew install cask room/cask/brew-cask
 brew cask install iterm2
 brew install -y zsh git vim
 
-sudo echo "$(which zsh)" >> /etc/shells
-
+# vim 에디터 설정
+sudo echo 'set number' >> ${HOME}/.vimrc
+sudo echo 'set backspace=indent,eol,start' >> ${HOME}/.vimrc
 
 # 기본 쉘의 위치를 바꾼다.
+sudo echo "$(which zsh)" >> /etc/shells
+
 chsh -s $(which zsh) 
 
 echo $SHELL
 
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+
 
 # Powerline fonts 설치 clone
 git clone https://github.com/powerline/fonts.git --depth=1
@@ -27,13 +33,20 @@ cd fonts
 cd ..
 rm -rf fonts
 
-
+# zsh 플러그인 설치
 brew install zsh-syntax-highlighting
-
-sudo echo 'set number' >> ${HOME}/.vimrc
-
 sudo echo 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ${HOME}/.zshrc
 
+ # autojump 설치 후 설정
+brew install autojump
+echo "[[ -s $(brew --prefix)/etc/profile.d/autojump.sh ]] && . $(brew --prefix)/etc/profile.d/autojump.sh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+
+# autosuggestions 설치 후 설정
+brew install zsh-autosuggestions
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc           
+
+# oh-my-zsh 프롬프트에 기본으로 표시되는 사용자 이름 삭제하기
 sudo echo '
 prompt_context() {
   if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
@@ -42,28 +55,37 @@ prompt_context() {
 }
 ' >> ${HOME}/.zshrc
 
-
-
+# 멀티라인 적용하기
 sudo echo '
 prompt_newline() {
   if [[ -n $CURRENT_BG ]]; then
-    echo -n "%{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{%k%F{blue}%}$SEGMENT_SEPARATOR"
+    echo -n "%{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR
+%(?.%F{$CURRENT_BG}.%F{red})❯%f"
+
   else
     echo -n "%{%k%}"
   fi
+
   echo -n "%{%f%}"
   CURRENT_BG=''
 }
 ' >> ${HOME}/.zshrc
 
-sudo echo 'alias python="python3"' >> ${HOME}/.zshrc
+# zshrc 재시작
+source ${ZDOTDIR:-$HOME}/.zshrc
 
-source ${HOME}/.zshrc
-
-# 필요 앱  설치
+# 필요 앱 설치
 brew cask install visual-studio-code
 brew cask install android-file-transfer simplenote google-chrome karabiner-elements alfred spectacle flux
-#brew cask install macx-youtube-downloader youtube-to-mp3
+# brew cask install macx-youtube-downloader youtube-to-mp3
+
+# wget https://download.docker.com/mac/stable/Docker.dmg
+# open Docker.dmg 
+# rm -rf Docker.dmg
+
+# wget https://download.docker.com/mac/stable/DockerToolbox.pkg
+# open DockerToolbox.pkg
+# rm -rf DockerToolbox.pkg
 
 reboot
 
--- node-install.sh
+++ node-install.sh
@@ -4,8 +4,10 @@ sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.s
 sudo echo 'export NVM_DIR="$HOME/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ${HOME}/.zshrc
 
+sudo echo 'alias npmg="npm ls -g --depth=0"' >> ${HOME}/.zshrc
+
 source ${HOME}/.zshrc
 
 nvm install node --lts
 
-npm install -g bower babel-cli eslint eslint-cli
+npm install -g yarn babel-cli eslint eslint-cli hexo-cli pm2 nodemon create-react-app
