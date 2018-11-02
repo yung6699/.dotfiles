e308f5e917ecb3f74791cfa71847ede9ad99ddd3
TaeYoung Yoon <yoon@TaeYoungui-MacBook-Pro.local>
2018-11-03 02:21:13 +0900

    update

--- default.sh
+++ default.sh
@@ -6,23 +6,21 @@ defaults write -g QLPanelAnimationDuration -float 0;killall Finder
 defaults write com.apple.Dock autohide-delay -float 0;killall Dock
 defaults write com.apple.dock expose-animation-duration -float 0.15;killall Dock
 
-
 # homebrew 설치
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew doctor
 brew update
-brew install -y zsh vim curl wget
+brew install -y vim curl wget
+brew install zsh
 
 # cask 설치
-brew install cask room/cask/brew-cask2
-
-# vim 에디터 설정
-sudo echo 'set number' >> ${ZDOTDIR:-$HOME}/.vimrc
-sudo echo 'set backspace=indent,eol,start' >> ${ZDOTDIR:-$HOME}/.vimrc
-
+brew tap caskroom/cask
+ 
 # zsh 쉘 위치 찾아 적용
+sudo chmod 777 /etc/shells
 sudo echo "$(which zsh)" >> /etc/shells
+sudo chmod 644 /etc/shells
 
 # 기본 쉘의 위치를 바꾼다.
 chsh -s $(which zsh) 
@@ -30,46 +28,8 @@ chsh -s $(which zsh)
 # 쉘 확인
 echo $SHELL
 
-# zshrc 재시작
-source ${ZDOTDIR:-$HOME}/.zshrc
+# vim 에디터 설정
+sudo echo 'set number' >> ${ZDOTDIR:-$HOME}/.vimrc
+sudo echo 'set backspace=indent,eol,start' >> ${ZDOTDIR:-$HOME}/.vimrc
 
-# oh-my-zsh 설치
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-# Powerline fonts 설치 clone
-git clone https://github.com/powerline/fonts.git --depth=1
-
-# install
-cd fonts
-./install.sh
-
-# clean-up a bit
-cd ..
-rm -rf fonts
-
-# zsh 플러그인 설치
-brew install zsh-syntax-highlighting
-echo 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >>  ${ZDOTDIR:-$HOME}/.zshrc
-
-# autosuggestions 설치 후 설정
-brew install zsh-autosuggestions
-echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc           
-
-# 멀티라인 및 터미널 세팅
-echo "source ~/.dotfiles/zshValues" >> ${ZDOTDIR:-$HOME}/.zshrc
-
-
-# nvm 설치 및 node 설치
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-
-# zshrc 재시작
-source ${ZDOTDIR:-$HOME}/.zshrc
-
-# 필요 앱 설치
-brew cask install visual-studio-code
-brew cask install android-file-transfer google-chrome karabiner-elements spectacle flux Alfred
-brew cask install Itsycal Allkdic PDF-Reader AppCleaner
-# brew cask install macx-youtube-downloader youtube-to-mp3
-
-sudo reboot
 
---
+++ default02.sh
@@ -0,0 +1,45 @@
+# zshrc 재시작
+source ${ZDOTDIR:-$HOME}/.zshrc
+
+# oh-my-zsh 설치
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Powerline fonts 설치 clone
+git clone https://github.com/powerline/fonts.git --depth=1
+
+# install
+cd fonts
+./install.sh
+
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+# zsh 플러그인 설치
+brew install zsh-syntax-highlighting
+
+echo 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >>  ${ZDOTDIR:-$HOME}/.zshrc
+
+# autosuggestions 설치 후 설정
+brew install zsh-autosuggestions
+
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc           
+
+# 멀티라인 및 터미널 세팅
+echo "source ~/.dotfiles/zshValues" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+
+# nvm 설치 및 node 설치
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
+# zshrc 재시작
+source ${ZDOTDIR:-$HOME}/.zshrc
+
+# 필요 앱 설치
+brew cask install visual-studio-code
+brew cask install android-file-transfer google-chrome spectacle flux Alfred
+
+brew cask install macx-youtube-downloader youtube-to-mp3
+
+sudo reboot
+
